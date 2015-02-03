ee47412d98d9637046a7096f24809bc45f6bcbd5
Quinn Slack <qslack@qslack.com>
2015-02-03 03:00:30 -0800

    allow http 206

--- s3util/open.go
+++ s3util/open.go
@@ -26,7 +26,7 @@ func Open(url string, c *Config) (io.ReadCloser, error) {
 	if err != nil {
 		return nil, err
 	}
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != 200 && resp.StatusCode != http.StatusPartialContent {
 		return nil, newRespError(resp)
 	}
 	return resp.Body, nil
